08f2069b5f452a6e6eb49fac4b728ab5cec9fe80
tenisistaa <krzychu_ten@o2.pl>
2018-07-16 14:10:53 +0200

    contact animation 2

--- app.js
+++ app.js
@@ -7,16 +7,15 @@ var icon = document.querySelectorAll('footer>div>a>i');
 var menu = document.querySelectorAll('ul>li');
 menu[0].addEventListener('click', function(){
   // console.log(projects);
-  // if(aboutme.classList.remove("hide-elem")){
-    aboutme.classList.remove("hide-elem");
+  if(aboutme.classList.contains("hide-elem")){
     aboutme.classList.add("show-elem");
-  // }
-  projects.classList.add("hide-elem");
-  skills.classList.add("show-skills");
-  if(aboutme.classList.remove("hide-elem")){
     aboutme.classList.remove("hide-elem");
-    aboutme.classList.add("show-elem");
   }
+    skills.classList.add("show-elem");
+    skills.classList.remove("hide-elem");
+  projects.classList.add("hide-elem");
+  // skills.classList.add("show-skills");
+
   if(contact[0].classList.contains('contact-move1')){
     contact[0].classList.remove('contact-move1');
     contact[0].classList.add('contact-move1-remove');
@@ -36,27 +35,27 @@ menu[0].addEventListener('click', function(){
 })
 menu[2].addEventListener('click',function(){
   projects.classList.add("hide-elem");
-  // if(aboutme.classList.remove("show-elem")){
+  // if(aboutme.classList.contains("show-elem")){
     aboutme.classList.remove("show-elem");
     aboutme.classList.add("hide-elem");
   // }
-  // if(skills.classList.remove("show-skills")){
-    skills.classList.remove("show-skills");
-    skills.classList.add("hide-skills");
+  // if(skills.classList.contains("show-skills")){
+    skills.classList.remove("show-elem");
+    skills.classList.add("hide-elem");
   // }
-  // if(contact[0].classList.remove('contact-move1-remove')){
+  // if(contact[0].classList.contains('contact-move1-remove')){
     contact[0].classList.remove('contact-move1-remove');
     contact[0].classList.add('contact-move1');
   // }
-  // if(contact[1].classList.remove('contact-move2-remove')){
+  // if(contact[1].classList.contains('contact-move2-remove')){
     contact[1].classList.remove('contact-move2-remove');
     contact[1].classList.add('contact-move2');
   // }
-  // if(contact[2].classList.remove('contact-move3-remove')){
+  // if(contact[2].classList.contains('contact-move3-remove')){
     contact[2].classList.remove('contact-move3-remove');
     contact[2].classList.add('contact-move3');
   // }
-  // if(contact[3].classList.remove('contact-move4-remove')){
+  // if(contact[3].classList.contains('contact-move4-remove')){
     contact[3].classList.remove('contact-move4-remove');
     contact[3].classList.add('contact-move4');
   // }
